809f0bce2cebfa22221b0bac5b767f987c1c52c4
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2023-12-13 02:04:01 +0600

    search bar added

--- src/components/LeftSideBar/LeftSideBar.jsx
+++ src/components/LeftSideBar/LeftSideBar.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { HiMagnifyingGlass } from 'react-icons/hi2';
 
 const LeftSideBar = () => {
     return (
-        <div className='col-span-2 primary-bg h-screen p-[26px]'>
-            left sidebar
+        <div className='col-span-2 primary-bg h-screen py-[25px] px-[15px]'>
+            <div className='relative'>
+                <input
+                    type="text"
+                    placeholder='Search...'
+                    className='max-w-full bg-[#333333] rounded-md outline-none py-[4px] px-[10px]'
+                />
+                <button className='absolute right-2 top-2 active:scale-90 duration-200'>
+                    <HiMagnifyingGlass />
+                </button>
+
+                <hr className='my-[26px] border-[#797979]' />
+
+            </div>
         </div>
     );
 };
