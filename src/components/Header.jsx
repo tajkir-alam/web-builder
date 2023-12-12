dd0313ec1d1e5c7a48cfdd63ee4cb1a320cc4f3f
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2023-12-13 01:07:04 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='primary-bg text-white py-[14px] ps-[26px] font-semibold'>
+            Web Builder Editor
+        </header>
+    )
+};
+
+export default Header;
