c3b692f3900beedf64b38692810acf73974c4538
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2023-12-14 16:40:19 +0600

    toggle btn created

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -9,7 +9,12 @@ const RightSideBar = () => {
     const [showComponents, setShowComponents] = useState(true);
     const [value, setValue] = useState('');
 
-    console.log(value);
+    const [isToggled, setIsToggled] = useState(false);
+
+    const handleToggle = () => {
+        setIsToggled(!isToggled);
+    };
+
 
     const toolbarOptions = [
         [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
@@ -46,7 +51,29 @@ const RightSideBar = () => {
                 value={value}
                 onChange={setValue}
                 modules={modules}
-                className='mt-4 min-h-[250px] h-auto bg-[#2F3031] border border-[#575757] rounded' />
+                className='mt-4 min-h-[250px] h-auto bg-[#2F3031] border border-[#575757] rounded'
+            />
+
+            <div>
+                <div className='flex '>
+                    <p>Inline Text Editing</p>
+                    <div className="flex items-center justify-center">
+                        <button
+                            onClick={handleToggle}
+                            className={`relative w-12 h-6 rounded-full focus:outline-none transition-transform duration-300 transform ${isToggled ? 'bg-green-500' : 'bg-gray-300'
+                                }`}
+                        >
+                            <div
+                                className={`absolute top-1 w-4 h-4 bg-white rounded-full shadow-md duration-300 ${isToggled ? 'translate-x-[calc(100%+12px)]' : 'translate-x-1'
+                                    }`}
+                            />
+                        </button>
+                    </div>
+                </div>
+                <p className='text-sm text-[#707070]'>
+                    Allow rich text editing inline on the page by double clicking on text blocks.
+                </p>
+            </div>
         </div>
     );
 };
