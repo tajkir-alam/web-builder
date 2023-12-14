3e4d59cf7e583df6011e38d66f622c999a079606
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2023-12-14 17:26:26 +0600

    add item box initialize

--- src/components/MainBody/MainBody.jsx
+++ src/components/MainBody/MainBody.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 const MainBody = () => {
     return (
-        <div className=" secondary-bg  py-[50px] px-[80px]">
-            <h1 className=''>Hello world welcome to my next.js project</h1>
-        </div>
+        <section className=" secondary-bg  py-[50px] px-[80px]">
+            <h4 className='bg-[#2B2B2B] rounded-md text-sm py-3 ps-4'>Desktop</h4>
+
+            <div className="mt-8">
+
+                <button className='relative border border-[#5D96FF] h-36 w-full rounded-sm'>
+                    <div className='absolute -top-4 right-4 p-2 bg-[#5D96FF] rounded-full hover:bg-[#719df0] duration-200'>
+                    <AiOutlinePlus />
+                    </div>
+                </button>
+
+            </div>
+        </section>
     );
 };
 
