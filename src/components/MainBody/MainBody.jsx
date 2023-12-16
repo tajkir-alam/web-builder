e83154a8233eaecfe0c06dcb6fb9c4a7fb4cc2ce
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2023-12-16 21:03:16 +0600

    content hover border effect and mouse pointer effect added

--- src/components/MainBody/MainBody.jsx
+++ src/components/MainBody/MainBody.jsx
@@ -26,16 +26,18 @@ const MainBody = ({ value }) => {
                             layout='fill'
                             src='/ComponentsBannerImg/banner1.png'
                         />
-                        
+
                         <div className='absolute text-center inset-y-1/4 w-2/5 left-[30%]'>
-                            <h6 className='capitalize text-[13.412px] text-[#69764A] font-semibold'>gift guide</h6>
-                            <h3 className='text-[36.224px] text-[#536136] font-bold'>Here’s to Joy</h3>
-                            <p className='text-[8.567px] text-[#7A8365] mb-[16px]'>
+                            <h6 className='content-border capitalize text-[13.412px] text-[#69764A] font-semibold'>gift guide</h6>
+                            <h3 className='content-border text-[36.224px] text-[#536136] font-bold'>Here’s to Joy</h3>
+                            <p className='content-border text-[8.567px] text-[#7A8365] mb-[14px]'>
                                 There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour
                             </p>
-                            <button className='bg-[#536136] hover:bg-[#627241] py-[6px] text-sm px-[14px] rounded capitalize duration-300 active:scale-95'>
-                                Shop Gifts
-                            </button>
+                            <div className='content-border'>
+                                <button className='bg-[#536136] hover:bg-[#627241] py-[6px] text-sm px-[14px] rounded capitalize duration-300 active:scale-95'>
+                                    Shop Gifts
+                                </button>
+                            </div>
                         </div>
 
                         {/* Section remove button */}
