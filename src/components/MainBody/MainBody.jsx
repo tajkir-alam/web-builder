c6e859f005339ab5009b5750ca6aee9daec5bc72
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2023-12-16 20:44:46 +0600

    change next/image to next/legacy-image

--- src/components/MainBody/MainBody.jsx
+++ src/components/MainBody/MainBody.jsx
@@ -26,7 +26,7 @@ const MainBody = ({ value }) => {
                             layout='fill'
                             src='/ComponentsBannerImg/banner1.png'
                         />
-
+                        
                         <div className='absolute text-center inset-y-1/4 w-2/5 left-[30%]'>
                             <h6 className='capitalize text-[13.412px] text-[#69764A] font-semibold'>gift guide</h6>
                             <h3 className='text-[36.224px] text-[#536136] font-bold'>Here’s to Joy</h3>
@@ -57,7 +57,7 @@ const MainBody = ({ value }) => {
                         <AiOutlinePlus />
                     </button>
 
-                    <button onClick={() => dispatch(increaseCount())} className='absolute inset-1/2 -translate-x-1/2 -translate-y-1/3 w-fit h-fit whitespace-nowrap flex items-center gap-2 py-2 px-4 bg-[#5D96FF] rounded hover:bg-[#719df0] duration-200'>
+                    <button onClick={() => dispatch(increaseCount())} className='absolute inset-1/2 -translate-x-1/2 -translate-y-1/3 w-fit h-fit whitespace-nowrap flex items-center gap-2 py-2 px-4 bg-[#5D96FF] rounded hover:bg-[#78a0e9] duration-200'>
                         <AiOutlinePlus />
                         Add Block
                     </button>
