9bcfc88d7197ee48287b415620b0a5f07a771676
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2023-12-16 17:38:45 +0600

    demo hero section created

--- src/components/MainBody/MainBody.jsx
+++ src/components/MainBody/MainBody.jsx
@@ -26,13 +26,13 @@ const MainBody = ({ value }) => {
                             src='/ComponentsBannerImg/banner1.png'
                         />
 
-                        <div className='absolute'>
-                            <h6 className='capitalize'>gift guide</h6>
-                            <h3>Here’s to Joy</h3>
-                            <p>
+                        <div className='absolute text-center inset-y-1/3 w-2/5 left-[30%]'>
+                            <h6 className='capitalize text-[13.412px] text-[#69764A] font-semibold'>gift guide</h6>
+                            <h3 className='text-[36.224px] text-[#536136] font-bold'>Here’s to Joy</h3>
+                            <p className='text-[8.567px] text-[#7A8365] mb-[16px]'>
                                 There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour
                             </p>
-                            <button>
+                            <button className='bg-[#536136] hover:bg-[#627241] py-[6px] text-sm px-[14px] rounded capitalize duration-300 active:scale-95'>
                                 Shop Gifts
                             </button>
                         </div>
